d636d46c5a7989d558f260dbce3f394ba771662f
sagor <sagor.eee.cuet@gmail.com>
2023-06-01 23:53:34 +0600

    method argument validation and custom exception handling

--- restful-web-services/src/main/java/com/learn/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/learn/restfulwebservices/user/UserResource.java
@@ -1,5 +1,6 @@
 package com.learn.restfulwebservices.user;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -33,7 +34,7 @@ public class UserResource {
     }
 
     @PostMapping(value = "/users")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
         User savedUser = userDaoService.save(user);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
--- restful-web-services/src/main/java/com/learn/restfulwebservices/user/exception/CustomResponseEntityExceptionHandler.java
+++ restful-web-services/src/main/java/com/learn/restfulwebservices/user/exception/CustomResponseEntityExceptionHandler.java
@@ -1,14 +1,18 @@
 package com.learn.restfulwebservices.user.exception;
 
 import com.learn.restfulwebservices.user.UserNotFountException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @ControllerAdvice
 public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
@@ -34,4 +38,19 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
 
         return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
     }
+
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                               HttpHeaders headers,
+                                                               HttpStatusCode status,
+                                                               WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDateTime.now(),
+                ex.getFieldErrors().stream().map(fe -> fe.getDefaultMessage()).toList().toString(),
+                request.getDescription(false)
+        );
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
 }
