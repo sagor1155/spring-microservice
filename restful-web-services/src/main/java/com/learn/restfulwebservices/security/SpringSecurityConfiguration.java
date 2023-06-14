13c208379f53bc11dd96ebb8a5c1ade892313194
sagor <sagor.eee.cuet@gmail.com>
2023-06-15 01:27:13 +0600

    add custom security configuration

---
+++ restful-web-services/src/main/java/com/learn/restfulwebservices/security/SpringSecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.learn.restfulwebservices.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        // all request should be authenticated
+        httpSecurity.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        // if not authenticated, a web page is shown
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        // CSRF -> POST, PUT
+        httpSecurity.csrf().disable();
+
+        return httpSecurity.build();
+    }
+
+}
