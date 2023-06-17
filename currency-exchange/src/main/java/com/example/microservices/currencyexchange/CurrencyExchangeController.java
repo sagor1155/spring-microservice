9e90a0841b1519c08b91dfa97fe7f6c71cda31d8
sagor <sagor.eee.cuet@gmail.com>
2023-06-18 04:56:10 +0600

    add environment property in currency exchange so multiple instance of currency exchange can be distinguished based on port

--- currency-exchange/src/main/java/com/example/microservices/currencyexchange/CurrencyExchangeController.java
+++ currency-exchange/src/main/java/com/example/microservices/currencyexchange/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.example.microservices.currencyexchange;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +11,17 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping(value = "/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(
             @PathVariable String from,
             @PathVariable String to
     ) {
-        return new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(108));
+        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(108));
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+        return currencyExchange;
     }
 }
