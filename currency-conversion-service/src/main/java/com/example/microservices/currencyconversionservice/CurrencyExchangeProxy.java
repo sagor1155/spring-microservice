885232e7592757e09f977e2d095fe36729bffffe
sagor <sagor.eee.cuet@gmail.com>
2023-06-21 04:52:45 +0600

    enable load balancing using feign and eureka

--- currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -4,7 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+// let the load balancer and eureka naming server find the registered instances for you
+@FeignClient(name = "currency-exchange-service")
 public interface CurrencyExchangeProxy {
 
     @GetMapping(value = "/currency-exchange/from/{from}/to/{to}")
