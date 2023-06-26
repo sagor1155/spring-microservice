d63db4d11f25a7b078eb83201341305351d6dc5a
sagor <sagor.eee.cuet@gmail.com>
2023-06-27 03:23:10 +0600

    circuit breaker using resilience4j

--- currency-exchange-service/src/main/java/com/example/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/example/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,5 +1,6 @@
 package com.example.microservices.currencyexchangeservice;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +15,8 @@ public class CircuitBreakerController {
     Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "onFallback")
+//    @Retry(name = "sample-api", fallbackMethod = "onFallback")
+    @CircuitBreaker(name = "default", fallbackMethod = "onFallback")
     public String sampleApi() {
         logger.info("Invoked Sample API!");
         ResponseEntity<String> responseEntity = new RestTemplate()
